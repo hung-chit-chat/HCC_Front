868fd3e76ef99305d6d1636abe1b029998d316f1
eunwoo-levi <eunwoo1341@gmail.com>
2024-10-03 19:59:08 +0900

    initial commit

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,8 +1,5 @@
 import React, { Suspense } from "react";
 import { HashRouter, Route, Routes } from "react-router-dom";
-// import PrivateRoute from "./utility/privateRoute";
-// import { useSelector } from "react-redux";
-// import "../style/style.scss";
 
 const loading = (
   <div className="pt-3 text-center">
@@ -10,48 +7,16 @@ const loading = (
   </div>
 );
 
-// // Containers
-// const DefaultLayout = React.lazy(
-//   () => import("./components/layout/defaultLayout")
-// );
-
-// Pages
 const Login = React.lazy(() => import("./page/login/login"));
 
 const App = () => {
-  // const session = useSelector((state) => state.app.session);
   const session = "a";
 
   return (
     <HashRouter>
       <Suspense fallback={loading}>
         <Routes>
-          <Route
-            // exact
-            path="/login"
-            // name="Login Page"
-            element={session ? <Login /> : <Login />}
-            // element={<Login />}
-          />
-          {/* <Route
-            // exact
-            path="/"
-            // name="Login Page"
-            element={session ? <Login /> : <Login />}
-            // element={<Login />}
-          /> */}
-          {/* <Route
-            path="*"
-            // name="Home"
-            element={
-              <PrivateRoute
-                authenticated={session}
-                component={
-                  <DefaultLayout visible={undefined} setVisible={undefined} />
-                }
-              />
-            }
-          /> */}
+          <Route path="/login" element={session ? <Login /> : <Login />} />
         </Routes>
       </Suspense>
     </HashRouter>
--- front/src/page/feed/feed.tsx
+++ front/src/page/feed/feed.tsx
@@ -1,5 +1,22 @@
-const Feed = () => {
-  return <></>;
-};
+import Post from "../../components/Post";
 
-export default Feed;
+export default function FeedPage() {
+  return (
+    <main className="w-full min-h-screen flex flex-col items-center">
+      <div className="my-10">
+        <h1 className="text-3xl font-bold">피드 페이지</h1>
+      </div>
+      <div>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </main>
+  );
+}
