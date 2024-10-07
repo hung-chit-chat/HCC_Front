b66ff1156b1cd46899e78f7bf7e703c394b0d1ed
eunwoo-levi <eunwoo1341@gmail.com>
2024-10-07 18:08:03 +0900

    refactor: defaulatLayout.tsx

--- front/src/components/layout/defaultLayout.tsx
+++ front/src/components/layout/defaultLayout.tsx
@@ -8,10 +8,7 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="min-h-scree">
-      <header className=" px-72 py-10 flex justify-start bg-violet-50">
-        <h1 className="text-center text-2xl">My Common Header</h1>
-      </header>
+    <div className="">
       <Sidebar />
       <main className="pl-72 pt-10 ">{children}</main>
     </div>
