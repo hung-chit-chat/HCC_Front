087af570a41ff95e7d45bd4ec93cdf93ec92a99c
eunwoo-levi <eunwoo1341@gmail.com>
2024-10-28 15:32:41 +0900

    refactor(layout): change into responsive Ui

--- front/src/components/layout/defaultLayout.tsx
+++ front/src/components/layout/defaultLayout.tsx
@@ -1,4 +1,4 @@
-import Sidebar from "./Siderbar";
+import Sidebar from './Siderbar';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -6,12 +6,12 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="min-h-scree">
-      <header className=" px-72 py-10 flex justify-start bg-violet-50">
-        <h1 className="text-center text-2xl">My Common Header</h1>
+    <div className='w-full min-h-screen'>
+      <header className='ml-[80px] py-10 text-center bg-violet-50'>
+        <h1 className='text-2xl'>My Common Header</h1>
       </header>
       <Sidebar />
-      <main className="pl-72 pt-10 ">{children}</main>
+      <main className='ml-[80px] pt-10 '>{children}</main>
     </div>
   );
 };
